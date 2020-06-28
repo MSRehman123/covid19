77efb256852deb7f1125f99556c510dc85fb073f
MSRehman123 <khalid786101@gmail.com>
2020-06-28 17:01:54 +0530

    css adjustments

--- src/header/header.js
+++ src/header/header.js
@@ -25,8 +25,8 @@ export class header extends Component {
             <span class="ml-3 text-xl">Covid Vs World</span>
           </a>
           <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900" href="/">Home</a>
-            <a class="mr-5 hover:text-gray-900" href="/prevention">Prevention</a>
+            <a class="mr-5 text-indigo-500 hover:text-gray-900 focus:text-green-500" href="/">Home</a>
+            <a class="mr-5 text-indigo-500 hover:text-gray-900 focus:text-green-500" href="/prevention">Prevention</a>
             {/*<a class="mr-5 hover:text-gray-900">Third Link</a>
             <a class="mr-5 hover:text-gray-900">Fourth Link</a> */}
           </nav>
